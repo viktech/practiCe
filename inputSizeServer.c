cf78b412ccd55ed2e0055a3b4063e2e905d5a847
viktech <viktechcomputers@gmail.com>
2017-08-07 16:25:59 -0500

    Implemented socket creation

---
+++ inputSizeServer.c
@@ -0,0 +1,56 @@
+//Good ol' scanf and printf; also need this to handle file IO
+#include <stdio.h>
+//lol @ library full of std
+#include <stdlib.h>
+//Going to need this for time(), ctime() - which are in turn needed for tick management
+#include <time.h>
+//For IP addresses and ports
+#include <netinet/in.h>
+//***I need to learn what this does
+#include <arpa/inet.h>
+//Makes available functions connect(), socket(), bind(), listen(), accept()
+#include <sys/socket.h>
+//Makes available all kinds of useful datatypes.  Here we will use time_t, off_t
+#include <sys/types.h>
+//I need NULL, and might be a dependency for other includes.
+//I'll need to try removing this at different stages in implementation and see what it breaks :x
+#include <unistd.h>
+//I can handle errors in a nicely human-readable way by strerr(errno)
+#include <errno.h>
+//So I don't have to manually duct tape char into string-like functionality
+#include <string.h>
+
+//since I need to enter command line arguments to set server IP
+int main(int argc, char *argv[])
+{
+	int listenfd = 0, connfd = 0;
+	struct sockaddr_in serv_addr;
+	char sendBuffer[1024];
+	time_t ticks;
+//lets begin by creating the socket
+	listenfd = socket(AF_INET , SOCK_STREAM , 0);
+	if (listenfd == -1)
+	{
+		printf("Error:  Socket creation failed!!\n");
+	}
+	
+	memset(&serv_addr, '0', sizeof(serv_addr));
+	memset(sendBuffer, '0', sizeof(sendBuffer));
+	
+	//svr socket address internet family is set to address family internetwork (UDP,TCP,etc.)
+	serv_addr.sin_family = AF_INET;
+	//svr socket internet address is set to any host address, in network byte order
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	//svr socket address internet port is set to port 5000 in network byte order
+	serv_addr.sin_port = htons(5000);
+
+//next we bind the socket
+//lets begin listening for connections
+//ok, lets establish a connection
+//lets stream some data
+//time to close the stream
+//lets release the datastructures
+
+
+return 0;
+}
